c4f1e59c3e4f082ae0bf3f9ae2d6808d314a6253
Ayush <150340364+123-ayush@users.noreply.github.com>
2025-10-24 06:40:28 +0000

    operators and operations

---
+++ 01_basics/04_operations.js
@@ -0,0 +1,25 @@
+let value = 3;
+let negVal = -value;
+
+// console.log(negVal);
+
+// console.log(2+2); -> addition
+// console.log(2-2); -> subtraction
+// console.log(2*2); -> multiplication
+// console.log(2**2); -> power
+// console.log(2/2); -> division
+// console.log(2%2); -> modulas
+
+
+let str1 = "hello "
+let str2 = "ayush"
+let str3 = str1 + str2; // hello ayush
+// console.log(str3);
+
+console.log("1" + 2); // 12
+console.log(1 + "2"); // 12
+console.log("1" + 2 + 2); // 122
+console.log(1 + 2 + "2"); //32
+console.log(3 + 4 * 5 % 3);
+
+//learn about operators from mdn
