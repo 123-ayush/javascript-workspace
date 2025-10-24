c38ccee938a9b3ba368ab2a0455c5f6acbe9928c
Ayush <150340364+123-ayush@users.noreply.github.com>
2025-10-24 06:03:16 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,26 @@
+"use strict"; //prefer to write so that compiler get a msg to treat all js code as newer version
+
+// alert("hello") // alert is not defined in node js which we are using but in browser we can do 
+
+// code readability should be high
+
+let name = "Ayush"
+let age = 20
+let isLoggedIn = false 
+
+// datatypes:
+// number => 2^53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value => its an object (typeof)
+// undefined => it is undefined(typeof)
+
+let ex = null // not undefined, its null
+
+// symbol => uniqueness
+
+console.log(typeof null);
+console.log(typeof undefined);
+
+//objects
