cd7ea79245ec13351e9045e0b167a77ab9b3df81
Ayush <150340364+123-ayush@users.noreply.github.com>
2025-10-28 13:45:54 +0000

    date and time

--- 01_basics/08_date_time.js
+++ 01_basics/08_date_time.js
@@ -1,3 +1,27 @@
 //Read date and time from MDN
 
 let myDate = new Date()
+
+console.log(typeof myDate); // object
+
+console.log(myDate.toString()); // Tue Oct 28 2025 13:31:04 GMT+0000 (Coordinated Universal Time)
+
+console.log(myDate.toDateString()); // Tue Oct 28 2025
+
+console.log(myDate.toLocaleString()); // 10/28/2025, 1:31:04 PM
+
+console.log(myDate.toISOString()) // 2025-10-28T13:32:33.684Z
+
+console.log(myDate.toJSON()); // 2025-10-28T13:34:21.790Z
+
+console.log(myDate.toLocaleDateString()); // 10/28/2025
+
+console.log(myDate.toTimeString()); // 13:34:21 GMT+0000 (Coordinated Universal Time
+
+// const myNewDate = new Date(2024, 0, 31, 22, 56)
+//                         (year, month(0-indexed), date, hour(std-24), min, sec)
+
+const myNewDate = new Date("01-23-2025 05:35:00 AM")
+//                        ("MM-DD-YYYY HH-MM-SS AM/PM")
+
+console.log(myNewDate.toLocaleString());
