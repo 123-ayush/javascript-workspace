4cae0d659d5a8f0c792df2d7f4c1728b16f5ab9e
Ayush <150340364+123-ayush@users.noreply.github.com>
2025-11-04 04:11:42 +0000

    Arrays basics

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,15 @@
+//Declaration
+
+const arr = [0,1,2,3,4]
+const arr1 = [true, 0 , "ayush", 0.23] //can contain multiple datatypes
+
+const arr2 = new Array(1,2,3,4)
+
+//Access -> 0-based indexing
+
+arr2.push(5) // add at last
+arr2.pop() // remove from last
+arr2.unshift(11) // add at first
+arr2.shift() // remove from first
+
+console.log(arr2);
