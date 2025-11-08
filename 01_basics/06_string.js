dea679aeaca175d474121716ae7133634cf28950
Ayush <150340364+123-ayush@users.noreply.github.com>
2025-11-08 06:12:16 +0000

    array revise

--- 01_basics/06_string.js
+++ 01_basics/06_string.js
@@ -5,6 +5,38 @@
 
 //--------------------Template Literals---------------------------------
 
-let age = 10;
+// let age = 10;
 
-console.log(`The age is ${age}`); 
+// console.log(`The age is ${age}`); 
+
+
+// ----------------------String Methods---------------------------------
+
+// 1. Trim -> trim space from start and end but not update the real value
+
+let msg = '   hello   '
+console.log(`real msg: ${msg}`);
+
+let newMsg = msg.trim()
+console.log(`after trim but new var msg: ${newMsg}`);
+
+console.log(`after trim msg: ${msg}`);
+
+
+//No change can be made to real/original string . whenevr we try to do change new string created old remain same 
+
+// 2. toLowerCase() & toUpperCase()
+
+// 3. indexOf() -> return first occurence of some arg. or return -1.
+
+// Method chaining is something when you use multiple methods on one thing ex. str.trim().toUpperCase(), evaluated from l->r.
+
+// 4. Slice(st, end) -> give part of some string(idx can be negative)
+
+// 5. length -> return length of string
+
+// 6. replace(old, new) -> find first occurence of old and replace with new 
+
+// 7. repeat(num) -> return a string with num number of copies of a string
+
+
--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -1,5 +1,7 @@
 //Declaration
 
+// arrays are mutable
+
 const arr = [0,1,2,3,4]
 const arr1 = [true, 0 , "ayush", 0.23] //can contain multiple datatypes
 
@@ -13,3 +15,45 @@ arr2.unshift(11) // add at first
 arr2.shift() // remove from first
 
 console.log(arr2);
+
+//----------------Adding & Removing----------------------------------
+
+const arr3 = ["shaktimaan", "power-rangers", "krrish"]
+const arr4 = ["chotta bheem", "krishna", "ganesh"]
+
+// arr1.push(arr2)
+// const heroes = arr2.concat(arr1)
+// const allNew = [...arr1, ...arr2]
+
+
+const ar5 = [1, 2, 3, [4, 5, 6], 7, [8, 9, [10, 11]]]
+
+const anotherArray = arr.flat(Infinity)
+
+console.log(anotherArray);
+
+
+// -----------------------Methods-----------------------------
+
+//1. indexOf() -> return idx from array if exist else return -1;
+
+//2. includes() -> return true/false
+
+//3. concat() -> merge two arrays and create a new arrays not change in previous
+
+//4. reverse() -> reverse the original array
+
+//5. slice(st, end) -> same as strings, return new arrays
+
+//6. splice() -> read carefully............ ****, this performs all actions on original arrays
+
+//7. sort() -> it sort arrays by converting all value to string and then sorting
+
+
+//-------------------------------------Constant Arrays---------------------------------
+
+const ar = [1,2,4,5,5];
+
+// can change value of arrays, length of array but can assign a new array to this varible
+
+ar = [2,4,76,3,3] // this cant be done
