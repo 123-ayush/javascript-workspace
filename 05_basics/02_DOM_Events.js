c858906246e9592093e946ba57e471073fe99ad2
123-ayush <asahlawat5@gmail.com>
2025-11-22 23:41:21 +0530

    DOM events

--- 05_basics/02_DOM_Events.js
+++ 05_basics/02_DOM_Events.js
@@ -2,3 +2,18 @@
 
 // Dom Events -> Events are signals that something has occured.
 
+// Examples of events:
+
+// A user clicks a button
+
+// A user types in an input box
+
+// A webpage has loaded
+
+// The mouse moves
+
+// The user scrolls
+
+// An element changes
+
+// Whenever these actions happen, the browser fires an event, and you can write JavaScript that listens and responds to it.
