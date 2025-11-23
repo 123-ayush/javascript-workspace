79b67e0375028da1bc33c87481e4fe4212cacc8f
123-ayush <asahlawat5@gmail.com>
2025-11-23 21:02:05 +0530

    keyboard and input events

--- 05_basics/02_DOM_Events.js
+++ 05_basics/02_DOM_Events.js
@@ -17,3 +17,57 @@
 // An element changes
 
 // Whenever these actions happen, the browser fires an event, and you can write JavaScript that listens and responds to it.
+
+// ----------------------------------------------OnClick--------------------------------------------------
+
+let btn = document.querySelector("button");
+
+console.dir(btn);
+
+btn.onclick = function() {
+    console.log("Button was clicked!");
+}
+
+
+// --------------------------------------------OnMouseEnter-------------------------------------------------
+
+let btn = document.querySelectorAll("button");
+
+btn.onmouseenter = function() {
+    console.log("Mouse entered on the button");
+}
+
+
+// ------------------------------------------AddEventListener------------------------------------------------
+
+// event.addeventlistener(event, callback);
+
+btn.addEventListener("click", function() {
+    console.log("Button was clicked!");
+})
+
+// ---------------------------------------------------This---------------------------------------------------
+
+function sayhello(){
+    console.log("Hello!");
+    this.style.backgroundColor = aqua;
+}
+
+btn.addEventListener("click", sayhello);
+
+
+// ----------------------------------------------KeyBoard Events---------------------------------------------
+
+let inp = document.querySelector("input");
+
+inp.addEventListener("keydown", function(event){
+    if(event.code == "KeyU"){
+        console.log("Character will move forward!");
+    }else if(event.code == "KeyD"){
+        console.log("Character will move backward!");
+    }else if(event.code == "KeyL"){
+        console.log("Character will move left!");
+    }else if(event.code == "KeyR"){
+        console.log("Character will move right!");
+    }
+});
